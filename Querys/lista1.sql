0a09d240dba5f826b730abe73bec188116b3f7a2
Fe <54954812+febreno@users.noreply.github.com>
2023-02-13 22:22:00 -0300

    Update lista1.sql

--- Querys/lista1.sql
+++ Querys/lista1.sql
@@ -42,20 +42,32 @@ DECLARE
     valorCambio number(10,2) := 2.98;
     valorDolarParaReal number(10,2) := 5.16;
 BEGIN
-    valorReal := v(alorReal * valorDolarParaReal) + valorCambio;
+    valorReal := (valorReal * valorDolarParaReal) + valorCambio;
     DBMS_OUTPUT.PUT_LINE('R$: '||valorReal);
     DBMS_OUTPUT.PUT_LINE('$: '||valorDolar);
 END;
 
 
-/*4*/
+/*5*/
 DECLARE
-    valorDolar number(10,2);
-    valorReal number(10,2) := 10;
-    valorCambio number(10,2) := 2.98;
-    valorDolarParaReal number(10,2) := 5.16;
+    valorcompra number(10,2) := 1000.00;
+    parcela number(10,2) := 10;
+    juros number(10,2) := 0.3;
+    valorFinal number(10,2);
 BEGIN
-    valorReal := v(alorReal * valorDolarParaReal) + valorCambio;
-    DBMS_OUTPUT.PUT_LINE('R$: '||valorReal);
-    DBMS_OUTPUT.PUT_LINE('$: '||valorDolar);
+    valorPorParcela := ( valorcompra * juros) / parcela;
+    DBMS_OUTPUT.PUT_LINE('R$: '||valorPorParcela);
+END;
+
+/*6*/
+DECLARE
+    valorcompra number(10,2) := 1000.00;
+    parcela number(10,2) := 6;
+    
+BEGIN
+    IF NOT parcela LIKE 6 THEN
+        OPEN c1;
+      END IF;
+    valorPorParcela := ( valorcompra * juros) / parcela;
+    DBMS_OUTPUT.PUT_LINE('R$: '||valorPorParcela);
 END;
