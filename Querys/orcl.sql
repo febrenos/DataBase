b1945ca3fbe61c6c0ca9098945683569c773d06f
Fe <54954812+febreno@users.noreply.github.com>
2023-03-21 16:34:10 -0300

    Update orcl.sql

--- Querys/orcl.sql
+++ Querys/orcl.sql
@@ -1,9 +1,10 @@
-
+-- sequence 1
 CREATE sequence SQ_T_USUARIO INCREMENT BY 1 START WITH 1;
 CREATE TABLE T_USUARIO (cd_usuario number PRIMARY KEY, nm_usuario VARCHAR2(100));
 INSERT INTO T_USUARIO (cd_usuario, nm_usuario) VALUES (SQ_T_USUARIO.nextval, 'Ronqui');
 select * from T_USUARIO;
 
+-- sequence 2
 CREATE SEQUENCE user_seq START WITH 1;
 CREATE TABLE estacao_recarga (
     id_estacao INTEGER DEFAULT user_seq.nextval NOT NULL,
